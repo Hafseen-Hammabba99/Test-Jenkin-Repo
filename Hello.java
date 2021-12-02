e76072164695d129c94a76a3dcf57faf052400eb
Hafseen-Hammabba99 <95336695+Hafseen-Hammabba99@users.noreply.github.com>
2021-12-02 18:25:23 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
 public class Hello{
     public static void main(String[] args){
           System.out.println("Hello World");
+        int a=10,b=20;
+        int sum = a+b;
+        System.out.println("Sum="+sum);
     }
 }
